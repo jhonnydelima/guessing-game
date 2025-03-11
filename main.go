72ab2e3c65914c2b0624d779b168f3ec91c0c2d3
Jhonny Lima <lima.jhonny95@gmail.com>
2025-03-10 23:42:09 -0300

    feat: create mod and start main function

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Guessing Game")
+	fmt.Println("A random number will be generated. Try to guess it. The number is an integer between 0 and 100.")
+}
